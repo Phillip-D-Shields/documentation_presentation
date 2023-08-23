cbaf89894d4aa19cbde934fefb8183504e0337eb
josh-moss <61395957+josh-moss@users.noreply.github.com>
2023-08-24 11:03:18 +1200

    added solution for reverse string

---
+++ solutions/JoshMoss/reverse-string.js
@@ -0,0 +1,14 @@
+// TODO write an exported function thats passes the tests in .spec.js
+// TODO write jsdoc comments for the function
+
+// ? follow this template for jsdoc comments
+/**
+ * Reverses string
+ *
+ * @param {paramType} reverseString - string
+ * @returns {string}
+ */
+
+export function reverseString(str) {
+    return str.split('').reverse().join('')
+}
