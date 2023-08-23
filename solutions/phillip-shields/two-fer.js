3f8ba5238c0da13b7071fffda73d047054a20dce
phillip shields <matua.phillip.shields@gmail.com>
2023-08-24 10:38:49 +1200

    reasonable jsdocs

--- solutions/phillip-shields/two-fer.js
+++ solutions/phillip-shields/two-fer.js
@@ -1,9 +1,11 @@
 /**
  *
- * SAJKDJALSKDJLAK;SDJML;AS ML;ASMD;LAMS
+ * A function that takes a string input with a name and returns the string
+ * "One for X, one for me." where X is the input name or "you" if no name is
+ * given.
  *
- * @param {string} nameInput - description
- * @returns {string} description
+ * @param {string} nameInput - user name or default to "you"
+ * @returns {string} - "One for X, one for me."
  */
 
 export function twoFer(nameInput = "you") {
