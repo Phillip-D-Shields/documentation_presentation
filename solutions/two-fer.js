bdc2a2afe2a7e7842e04e8fdd9a0012e7ee5cc7d
Calzep <caleb-easpn@nmit.live.ac.nz>
2023-08-24 10:43:26 +1200

    Updateed comments

--- solutions/two-fer.js
+++ solutions/two-fer.js
@@ -1,15 +1,10 @@
-// TODO write an exported function that passes the tests in .spec.js
-// TODO write jsdoc comments for the function
-
-// ? follow this template for jsdoc comments
 /**
- * what does the function do?
+ *Takes a name input and returns the name in a sentance.  If no name is given will return 'you' in its place
  *
  * @param {string} nameInput - description
  * @returns {string} description
  */
 
-//Takes a name input and returns the name in a sentance.  If no name is given will return 'you' in its place
 
 export function twoFer(nameInput ='you'){
     return `One for ${nameInput}, one for me.`;
