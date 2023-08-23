6cfc2e0638d382f8f5ad0dfaa0ac50f03f79cb7f
josephcruel <Joseph-Cruel@live.nmit.ac.nz>
2023-08-24 10:28:38 +1200

    created twoFer function

--- src/twoFer/two-fer.js
+++ src/twoFer/two-fer.js
@@ -3,8 +3,14 @@
 
 // ? follow this template for jsdoc comments
 /**
- * what does the function do?
+ * If a user give a nameInput, it returns the new name in the function.
  *
  * @param {paramType} paramName - description
  * @returns {returnType} description
  */
+
+export function twoFer(nameInput = 'you') {
+    return `One for ${nameInput}, one for me.`
+}
+
+console.log(twoFer);
