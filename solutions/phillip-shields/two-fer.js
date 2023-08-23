977b1a1e4efedca9c14cbfd8b735edb9668d8607
phillip shields <matua.phillip.shields@gmail.com>
2023-08-24 10:30:03 +1200

    two fer solution and my directory

---
+++ solutions/phillip-shields/two-fer.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * SAJKDJALSKDJLAK;SDJML;AS ML;ASMD;LAMS
+ *
+ * @param {string} nameInput - description
+ * @returns {string} description
+ */
+
+export function twoFer(nameInput = "you") {
+  return `One for ${nameInput}, one for me.`;
+}
