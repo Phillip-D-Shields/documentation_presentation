04acfbda4ddf77fc86ccaf359a927fb8a63f8da3
josh-moss <61395957+josh-moss@users.noreply.github.com>
2023-08-24 10:29:13 +1200

    added twofer function to return string input

---
+++ solutions/JoshMoss/two-fer.js
@@ -0,0 +1,18 @@
+// TODO write an exported function that passes the tests in .spec.js
+// TODO write jsdoc comments for the function
+
+import { log } from "console"
+
+// ? follow this template for jsdoc comments
+/**
+ * comments
+ *
+ * @param {string}} name of the datatype
+ * @returns {nameInput} Name import
+ */
+
+export function twoFer(nameInput = 'you') {
+    return `One for ${nameInput}, one for me.`
+}
+
+console.log(twoFer('Josh'));
